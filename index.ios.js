312575d6789e6cfab6a97b8fdb739b90715396a8
Vinay Nair <vinay.nair@bookmyshow.com>
2017-03-30 16:52:25 +0530

    Making a network request to fetch a list of albums. Displaying a basic list of albums fetched.

--- index.ios.js
+++ index.ios.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 export default class Albums extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.viewStyle}>
         <Header />
+        <AlbumList />
       </View>
     );
   }
 }
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#b9c0cc'
+  }
+};
 
 AppRegistry.registerComponent('Albums', () => Albums);
